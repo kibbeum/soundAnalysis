75233f94e832545f20eea42620c8c1c94da5a2c1
kibbeum <gin12345@hanmail.net>
2021-02-06 02:18:43 +0900

    first commit

--- mplwidget.py
+++ mplwidget.py
@@ -1,14 +1,23 @@
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as Canvas
 import numpy as np
+from matplotlib.patches import Rectangle
+import matplotlib
 
+
+from matplotlib.widgets import RectangleSelector
+from matplotlib.widgets import SpanSelector
+
+"""
 import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QGroupBox, QHBoxLayout
+from PyQt5.QtGui import QPainter, QPixmap, QPen, QBrush, QImage, QIcon
+"""
 
-
+matplotlib.use('QT5Agg')
 
 # Matplotlib canvas class to create figure
 class MplCanvas(Canvas):
@@ -16,12 +25,6 @@ class MplCanvas(Canvas):
         self.fig = Figure()
         self.ax1 = self.fig.add_subplot(211)
         self.ax2 = self.fig.add_subplot(212)
-
-        self.lx = self.ax2.axhline(color='g')  # the horiz line
-        self.ly = self.ax2.axvline(color='g')  # the vert line
-        self.lx2 = self.ax2.axhline(color='y')  # the horiz line
-        self.ly2 = self.ax2.axvline(color='y')  # the vert line
-
 
 
         Canvas.__init__(self, self.fig)
@@ -41,45 +44,102 @@ class MplWidget(QtWidgets.QWidget):
         self.vbl.addWidget(self.canvas)
         self.setLayout(self.vbl)
 
+
+
         self.canvas.mpl_connect("button_press_event", self.on_press)
         self.canvas.mpl_connect("button_release_event", self.on_release)
         self.canvas.mpl_connect("motion_notify_event", self.on_move)
 
+        self.isPressd = False
+        self.rectStartX=0
+        self.rectStartY=0
+        self.rectEndX=0
+        self.rectEndY=0
+
+        self.RS = RectangleSelector(self.canvas.ax2, self.line_select_callback,
+                                       drawtype='box', useblit=True,
+                                       button=[1, 3],  # don't use middle button
+                                       minspanx=5, minspany=5,
+                                       spancoords='pixels',
+                                       interactive=True)
+
+        """
+        self.span = SpanSelector(self.canvas.ax1, self.onselect,
+                                 'horizontal', useblit=True,
+                                 button=[1],  # don't use middle button
+                                rectprops=dict(alpha=0.5, facecolor='red'), span_stays=True)
+        
+    def onselect(self, xmin, xmax):
+        xmin
+        #indmin, indmax = np.searchsorted(x, (xmin, xmax))
+        #indmax = min(len(x) - 1, indmax)
+        #thisx = x[indmin:indmax]
+        #thisy = y[indmin:indmax]
+        #line2.set_data(thisx, thisy)
+        #ax2.set_xlim(thisx[0], thisx[-1])
+        #ax2.set_ylim(thisy.min(), thisy.max())
+        #fig.canvas.draw()
+    """
+
+    def line_select_callback(self, eclick, erelease):
+        self.rectStartX = 0
+        self.rectStartY = 0
+        self.rectEndX = 0
+        self.rectEndY = 0
+
+
     def on_press(self, event):
+        self.isPressd = True
+        self.rectStartX = event.xdata
+        self.rectStartY = event.ydata
+        self.rectEndX = event.xdata
+        self.rectEndY = event.ydata
         print("press")
         print("event.xdata", event.xdata)
         print("event.ydata", event.ydata)
 
-        self.canvas.lx.set_ydata(event.ydata)
-        self.canvas.ly.set_xdata(event.xdata)
-        self.canvas.draw()
-
     def on_release(self, event):
+        self.isPressd = False
+        self.rectStartX = 0
+        self.rectStartY = 0
+        self.rectEndX = 0
+        self.rectEndY = 0
         print("release:")
         print("event.xdata", event.xdata)
         print("event.ydata", event.ydata)
-        self.canvas.lx2.set_ydata(event.ydata)
-        self.canvas.ly2.set_xdata(event.xdata)
-        self.canvas.draw()
+        #self.canvas.draw()
+
+
+
 
     def on_move(self, event):
+        if(self.isPressd is True):
+            self.rectEndX = event.xdata
+            self.rectEndY = event.ydata
+
+
         if(event.inaxes==self.canvas.ax1):
             if (isinstance(event.xdata, float) and isinstance(event.ydata, float)):
-                self.parent().parent().label1.setText("Time: " + str(np.round(event.xdata, 2)) + "s")
-                self.parent().parent().label2.setText("Min: " + str(np.round(event.ydata, 2)) )
-                self.parent().parent().label3.setText("Max: "  )
+                self.parent().parent().label1.setText("Time: " + str(np.round(event.xdata, 2)) + "s   " +
+                                                      "Min: " + str(np.round(event.ydata, 2)) + "   " +
+                                                      "Max: ")
+                #self.parent().parent().label2.setText("Min: " + str(np.round(event.ydata, 2)) )
+                #self.parent().parent().label3.setText("Max: "  )
+
+
         if (event.inaxes == self.canvas.ax2):
             if (isinstance(event.xdata, float) and isinstance(event.ydata, float)):
-                self.parent().parent().label1.setText("Time: " + str(np.round(event.xdata, 2)) + "s")
-                self.parent().parent().label2.setText("Freq: " + str(np.round(event.ydata, 2)) + "Hz")
-
                 #######################################################
                 # stft spectrum data (x, y)
                 # x : input_x * sr * amplitude_x_size / audio_y size
                 # y : input_y * amplitude_y_size * 2 / sr
                 sptx = int(event.xdata * self.audio_sr * np.shape(self.spectrum)[0] / np.size(self.audio_y))
                 spty = int(event.ydata * np.shape(self.spectrum)[1] * 2 / self.audio_sr)
-                self.parent().parent().label3.setText("Power: "+ str(self.spectrum[sptx][spty]) + "dB")
+                self.parent().parent().label1.setText("Time: " + str(np.round(event.xdata, 3)) + "s   " +
+                                                      "Freq: " + str("{:,}".format(int(event.ydata))) + "Hz   " +
+                                                      "Power: " + str(np.round(self.spectrum[sptx][spty], 1)) + "dB")
+                #self.parent().parent().label2.setText("Freq: " + str(np.round(event.ydata, 2)) + "Hz")
+                #self.parent().parent().label3.setText("Power: "+ str(self.spectrum[sptx][spty]) + "dB")
 
 
 
@@ -90,11 +150,33 @@ class MplWidget(QtWidgets.QWidget):
 
 
 
+"""
+from PyQt5.QtWidgets import QApplication, QWidget, QAction, QLabel, QMainWindow
+from PyQt5.QtGui import QPainter, QPixmap, QPen, QBrush, QImage, QIcon
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtCore import Qt, QPoint
+
+           pen = QtGui.QPen()
+            pen.setWidth(3)
+            pen.setColor(QtGui.QColor(255, 0, 0))
+
+            brush = QtGui.QBrush()
+            brush.setColor(QtGui.QColor(255, 0, 0))
+            brush.setStyle(Qt.SolidPattern)
+
+            painter.setBrush(brush)
+            painter.setPen(pen)
+            painter.drawRect(207, 152, 409, 222)
+            painter.end()
+            
+                qp.setPen(QPen(Qt.black, 3, Qt.DotLine))
+    qp.drawLine(20, 120, 380, 120)
+
+"""
 
 
 
-
-
+"""
 class Cursor(object):
     def __init__(self, ax):
         self.ax = ax
@@ -117,3 +199,4 @@ class Cursor(object):
 
         self.txt.set_text('x=%1.2f, y=%1.2f' % (x, y))
         self.ax.figure.canvas.draw()
+"""
